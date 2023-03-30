e809b09b6692c91956f3ff9094a08bccc50eab3b
hulakk <hulakk@outlook.com>
2023-03-30 14:29:04 +0800

    2023/03/30 周四 14:29:04.65

--- merge_and_push.bat
+++ merge_and_push.bat
@@ -1,6 +1,12 @@
 @REM 将_site文件夹重命名为docs，以便在Github Page上部署
 mv _site docs
 
+@REM 切换分支
+git checkout main
+
+@REM dev合并到当前分支
+git merge dev
+
 git add -A
 git commit -m "%date% %time%"
 git push origin main
